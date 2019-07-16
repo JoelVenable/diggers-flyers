41f16c3d5560c977ea98c335f27281253bcb074e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-16 13:59:07 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diggers_flyers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
