190df502e3d0887cbce0d7990d4877fb90a40d37
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-16 14:55:33 -0500

    Instantiated list classes

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ namespace diggers_flyers
     {
         static void Main(string[] args)
         {
-            
+            Aquarium myAquarium = new Aquarium();
+            Cage myCage = new Cage();
+            Aviary myAviary = new Aviary();
+            SnakePen mySnakePen = new SnakePen();
+            Terrarium myTerrarium = new Terrarium();
 
-            
+
 
         }
     }
