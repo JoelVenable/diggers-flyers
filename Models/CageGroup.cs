0880c8a708b96d66fd0bbf9ab897f314f3ea6004
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-16 14:47:35 -0500

    Corrected naming convention

--- Models/CageGroup.cs
+++ Models/CageGroup.cs
@@ -8,26 +8,26 @@ namespace diggers_flyers.Models
 {
     class CageGroup
     {
-        public List<IFlyable> birdCage { get; set; } = new List<IFlyable>() {
+        public List<IFlyable> BirdCage { get; set; } = new List<IFlyable>() {
         new Finch(),
         new Parakeet(),
         };
 
 
-        public List<ISlitherable> snakeCage { get; set; } = new List<ISlitherable>()
+        public List<ISlitherable> SnakeCage { get; set; } = new List<ISlitherable>()
         {
             new CopperheadSnake(),
             new TimberRattlesnake(),
         };
 
-        public List<IDiggable> crawlies { get; set; } = new List<IDiggable>()
+        public List<IDiggable> Crawlies { get; set; } = new List<IDiggable>()
         {
             new Ant(),
             new Earthworm(),
             new Mouse()
         };
 
-        public List<ISwimmable> aquarium { get; set; } = new List<ISwimmable>()
+        public List<ISwimmable> Aquarium { get; set; } = new List<ISwimmable>()
         {
             new BetaFish(),
             new Terrapin()
