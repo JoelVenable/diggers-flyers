87f7a5626dc3008a77007974a71c438bdd48e755
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-16 14:46:03 -0500

    Added slitherable interface

---
+++ Interfaces/ISlitherable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace diggers_flyers.Interfaces
+{
+    interface ISlitherable
+    {
+        void Slither();
+    }
+}
